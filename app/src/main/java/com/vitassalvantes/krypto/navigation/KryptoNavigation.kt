2e1f9ed012b6736a6b438d60c5c71e982961422d
VitasSalvantes <salvantesvitas@gmail.com>
2021-10-03 17:30:56 +0200

    Created file that contains BottomAppBar with FloatActionButton

---
+++ app/src/main/java/com/vitassalvantes/krypto/navigation/KryptoNavigation.kt
@@ -0,0 +1,83 @@
+package com.vitassalvantes.krypto.navigation
+
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.vitassalvantes.krypto.ui.theme.Shapes
+
+@Composable
+fun KryptoBottomAppBar(kryptoNavController: NavController) {
+
+    BottomAppBar(cutoutShape = Shapes.small) {
+        BottomNavigation {
+            val navBackStackEntry by kryptoNavController.currentBackStackEntryAsState()
+            val currentDestination = navBackStackEntry?.destination
+            val items = listOf(
+                KryptoScreen.RoomsScreen,
+                KryptoScreen.CiphersScreen
+            )
+
+            items.forEach { kryptoScreen ->
+                BottomNavigationItem(
+                    icon = {
+                        Icon(
+                            kryptoScreen.iconId,
+                            contentDescription = stringResource(id = kryptoScreen.labelId)
+                        )
+                    },
+                    label = { Text(text = stringResource(id = kryptoScreen.labelId)) },
+                    selected = currentDestination?.hierarchy?.any { it.route == kryptoScreen.route } == true,
+                    onClick = {
+                        kryptoNavController.navigate(kryptoScreen.route) {
+                            // Pop up to the start destination of the graph to
+                            // avoid building up a large stack of destinations
+                            // on the back stack as users select items
+                            popUpTo(kryptoNavController.graph.findStartDestination().id) {
+                                saveState = true
+                            }
+                            // Avoid multiple copies of the same destination when
+                            // reselecting the same item
+                            launchSingleTop = true
+                            // Restore state when reselecting a previously selected item
+                            restoreState = true
+                        }
+                    }
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun KryptoFloatingActionButton() {
+    FloatingActionButton(onClick = { /*TODO Create a new room*/ }, shape = Shapes.small) {
+        Icon(Icons.Filled.Add, "Create a new room")
+    }
+}
+
+@Preview(
+    name = "KryptoBottomAppBar",
+    showBackground = true
+)
+@Composable
+fun PreviewKryptoBottomAppBar() {
+    KryptoBottomAppBar(kryptoNavController = rememberNavController())
+}
+
+@Preview(
+    name = "KryptoFloatingActionButton",
+    showBackground = true
+)
+@Composable
+fun PreviewKryptoFloatingActionButton() {
+    KryptoFloatingActionButton()
+}
