9140f2a8234e7776519791e9f999e4de6deab657
VitasSalvantes <salvantesvitas@gmail.com>
2021-10-03 17:28:44 +0200

    Created screen that contains all rooms

---
+++ app/src/main/java/com/vitassalvantes/krypto/ui/screens/RoomsScreen.kt
@@ -0,0 +1,9 @@
+package com.vitassalvantes.krypto.ui.screens
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun RoomsScreen() {
+    Text(text = "Rooms Screen")
+}
