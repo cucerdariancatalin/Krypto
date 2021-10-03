d009af09688d72eae25941a39bf52cf404898ab5
VitasSalvantes <salvantesvitas@gmail.com>
2021-10-03 17:26:33 +0200

    Created screen that contains all ciphers

---
+++ app/src/main/java/com/vitassalvantes/krypto/ui/screens/CiphersScreen.kt
@@ -0,0 +1,9 @@
+package com.vitassalvantes.krypto.ui.screens
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CiphersScreen() {
+    Text(text = "Ciphers Screen")
+}
