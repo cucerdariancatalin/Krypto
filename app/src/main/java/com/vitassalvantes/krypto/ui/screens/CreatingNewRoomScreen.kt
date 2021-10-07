a12b19adfff1d1fc6bf8761128b444190c069c68
VitasSalvantes <salvantesvitas@gmail.com>
2021-10-07 13:04:42 +0200

    Created CreatingNewRoomScreen

---
+++ app/src/main/java/com/vitassalvantes/krypto/ui/screens/CreatingNewRoomScreen.kt
@@ -0,0 +1,10 @@
+package com.vitassalvantes.krypto.ui.screens
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import com.vitassalvantes.krypto.KryptoViewModel
+
+@Composable
+fun CreatingNewRoomScreen(viewModel: KryptoViewModel) {
+    Text(text = "CreatingNewRoomScreen")
+}
