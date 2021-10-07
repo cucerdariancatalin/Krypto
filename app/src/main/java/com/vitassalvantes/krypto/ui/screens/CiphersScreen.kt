e2f772a80dbba2c5531c5c372870f7fa3782836e
VitasSalvantes <salvantesvitas@gmail.com>
2021-10-07 13:34:35 +0200

    Removed view model

--- app/src/main/java/com/vitassalvantes/krypto/ui/screens/CiphersScreen.kt
+++ app/src/main/java/com/vitassalvantes/krypto/ui/screens/CiphersScreen.kt
@@ -2,9 +2,8 @@ package com.vitassalvantes.krypto.ui.screens
 
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import com.vitassalvantes.krypto.KryptoViewModel
 
 @Composable
-fun CiphersScreen(viewModel: KryptoViewModel) {
+fun CiphersScreen() {
     Text(text = "Ciphers Screen")
 }
