c6d50ec19812db49ccbf58386db0a25da5a88244
VitasSalvantes <salvantesvitas@gmail.com>
2021-10-11 16:28:44 +0200

    Added comments

--- app/src/main/java/com/vitassalvantes/krypto/KryptoViewModel.kt
+++ app/src/main/java/com/vitassalvantes/krypto/KryptoViewModel.kt
@@ -3,14 +3,26 @@ package com.vitassalvantes.krypto
 import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
 
+/**
+ * [KryptoViewModel] holds information about a all rooms and manages it.
+ */
 class KryptoViewModel : ViewModel() {
+    /**
+     * List of all rooms
+     */
     var listOfRooms = mutableStateListOf<KryptoRoom>()
         private set
 
+    /**
+     * Adding a new room to [listOfRooms]
+     */
     fun addNewRoom(newRoom: KryptoRoom) {
         listOfRooms.add(newRoom)
     }
 
+    /**
+     * Removing a room from [listOfRooms]
+     */
     fun removeRoom(roomToRemove: KryptoRoom) {
         listOfRooms.remove(roomToRemove)
     }
