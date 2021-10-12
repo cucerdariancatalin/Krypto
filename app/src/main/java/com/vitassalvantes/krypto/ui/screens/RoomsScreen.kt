963db3efa21742bc106e71f4db7384afed210c8c
VitasSalvantes <salvantesvitas@gmail.com>
2021-10-12 13:11:34 +0200

    Added dialog for editing or deleting room

--- app/src/main/java/com/vitassalvantes/krypto/ui/screens/RoomsScreen.kt
+++ app/src/main/java/com/vitassalvantes/krypto/ui/screens/RoomsScreen.kt
@@ -1,10 +1,22 @@
 package com.vitassalvantes.krypto.ui.screens
 
+import android.widget.Toast
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
@@ -17,28 +29,78 @@ import com.vitassalvantes.krypto.ui.components.KryptoCard
  */
 @Composable
 fun RoomsScreen(viewModel: KryptoViewModel, navController: NavHostController) {
+    // Boolean variable to display the dialog
+    var openDialog by rememberSaveable { mutableStateOf(false) }
+
+    // Index of long pressed room for dialog
+    var pressedRoomIndex by rememberSaveable { mutableStateOf(0) }
+
+    // Context for Toast
+    val context = LocalContext.current
+
+    // Dialog for editing or deleting the room
+    if (openDialog) {
+        AlertDialog(
+            onDismissRequest = { openDialog = !openDialog },
+            title = { Text(text = "Delete room or edit?") },
+            buttons = {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                    horizontalArrangement = Arrangement.End
+                ) {
+                    TextButton(onClick = {
+                        Toast.makeText(
+                            context,
+                            "Removed ${viewModel.listOfRooms[pressedRoomIndex].name}",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        viewModel.removeRoom(viewModel.listOfRooms[pressedRoomIndex])
+                        openDialog = !openDialog
+                    }) {
+                        Text(text = "Remove".uppercase())
+                    }
+
+                    TextButton(onClick = { /*TODO*/ }) {
+                        Text(text = "Edit".uppercase())
+                    }
+                }
+            })
+    }
+
     LazyColumn(Modifier.padding(16.dp)) {
         items(viewModel.listOfRooms) { room ->
             KryptoCard(
                 cardName = room.name,
                 cardIcon = room.cipher.icon,
-            ) {
-                navController.navigate(
-                    KryptoScreen.RoomDetailsScreen.route + "/${viewModel.listOfRooms.indexOf(room)}"
-                ) {// Pop up to the start destination of the graph to
-                    // avoid building up a large stack of destinations
-                    // on the back stack as users select items
-                    popUpTo(navController.graph.findStartDestination().id) {
-                        saveState = true
-                    }
-                    // Avoid multiple copies of the same destination when
-                    // reselecting the same item
-                    launchSingleTop = true
+                onClickListener = {
+                    navController.navigate(
+                        KryptoScreen.RoomDetailsScreen.route + "/${
+                            viewModel.listOfRooms.indexOf(
+                                room
+                            )
+                        }"
+                    ) {// Pop up to the start destination of the graph to
+                        // avoid building up a large stack of destinations
+                        // on the back stack as users select items
+                        popUpTo(navController.graph.findStartDestination().id) {
+                            saveState = true
+                        }
+                        // Avoid multiple copies of the same destination when
+                        // reselecting the same item
+                        launchSingleTop = true
 
-                    // Restore state when reselecting a previously selected item
-                    restoreState = false
+                        // Restore state when reselecting a previously selected item
+                        restoreState = false
+                    }
+                },
+                onLongClickListener = {
+                    // Call the dialog
+                    openDialog = !openDialog
+                    pressedRoomIndex = viewModel.listOfRooms.indexOf(room)
                 }
-            }
+            )
         }
     }
 }
