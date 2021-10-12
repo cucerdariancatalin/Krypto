4b7fada704f05f5c4d449e784abbbfa6b5274576
VitasSalvantes <salvantesvitas@gmail.com>
2021-10-12 10:40:44 +0200

    Implemented the Caesar cipher

--- app/src/main/java/com/vitassalvantes/krypto/ciphers/CaesarCipher.kt
+++ app/src/main/java/com/vitassalvantes/krypto/ciphers/CaesarCipher.kt
@@ -5,17 +5,49 @@ import androidx.compose.material.icons.filled.Edit
 import androidx.compose.ui.graphics.vector.ImageVector
 import com.vitassalvantes.krypto.R
 
+/**
+ * Class for encrypting messages using the Caesar cipher
+ */
 class CaesarCipher(
     name: String = "Caesar Cipher",
     description: Int = R.string.caesar_cipher_description,
     icon: ImageVector = Icons.Filled.Edit
 ) : KryptoCipher(name = name, description = description, icon = icon) {
 
+    /**
+     * This method encrypts a [message] with the help of a [key].
+     */
     override fun encrypt(message: String, key: String): String {
-        TODO("Not yet implemented")
+        val keyNumber = keyToInt(key)
+
+        //First, the string is converted to a character array.
+        //Then a key is added to the Unicode number of each element in the array.
+        //The resulting array is converted back to a string.
+        return message.toCharArray().map { it.plus(keyNumber) }.joinToString(separator = "")
     }
 
+    /**
+     * This method decrypts a [message] with the help of a [key].
+     */
     override fun decrypt(message: String, key: String): String {
-        TODO("Not yet implemented")
+        val keyNumber = keyToInt(key)
+
+        //First, the string is converted to a character array.
+        //Then a key is subtracted from the Unicode number of each element in the array.
+        //The resulting array is converted back to a string.
+        return message.toCharArray().map { it.minus(keyNumber) }.joinToString(separator = "")
+    }
+
+    /**
+     * For this cipher, the [key] must be converted to Int.
+     */
+    private fun keyToInt(key: String): Int {
+        var keyNumber = 0
+
+        //First, the string is converted to a character array.
+        //Then the Unicode number of each element in the array is added to keyNumber.
+        //The resulting array is converted back to a string.
+        key.toCharArray().forEach { keyNumber += it.code }
+        return keyNumber
     }
 }
