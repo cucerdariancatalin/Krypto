da5384b71a662015997cb5d914147487c9322f4d
VitasSalvantes <salvantesvitas@gmail.com>
2021-10-17 11:19:19 +0200

    Added method getCipher

--- app/src/main/java/com/vitassalvantes/krypto/ciphers/CiphersInfo.kt
+++ app/src/main/java/com/vitassalvantes/krypto/ciphers/CiphersInfo.kt
@@ -1,5 +1,7 @@
 package com.vitassalvantes.krypto.ciphers
 
+import androidx.annotation.StringRes
+
 /**
  * Singleton to access the list of all implemented ciphers
  */
@@ -10,4 +12,15 @@ object CiphersInfo {
     val listOfAllCiphers = listOf<KryptoCipher>(
         CaesarCipher()
     )
+
+    /**
+     * Getting a cipher by its [com.vitassalvantes.krypto.ciphers.KryptoCipher.name]
+     */
+    fun getCipher(@StringRes cipherName: Int): KryptoCipher {
+        listOfAllCiphers.forEach {
+            if (it.name == cipherName) return it
+        }
+        // TODO: 17.10.2021 throw CipherNotFoundException
+        return listOfAllCiphers[0]
+    }
 }
