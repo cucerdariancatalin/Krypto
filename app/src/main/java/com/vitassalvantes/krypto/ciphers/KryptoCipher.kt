f9a6d5a55d6784889b26b38255b1429ad88e3d60
VitasSalvantes <salvantesvitas@gmail.com>
2021-10-17 10:37:21 +0200

    Added comments and changed name type to Int

--- app/src/main/java/com/vitassalvantes/krypto/ciphers/KryptoCipher.kt
+++ app/src/main/java/com/vitassalvantes/krypto/ciphers/KryptoCipher.kt
@@ -3,13 +3,35 @@ package com.vitassalvantes.krypto.ciphers
 import androidx.annotation.StringRes
 import androidx.compose.ui.graphics.vector.ImageVector
 
+/**
+ * A cipher with a [name], [description] and methods to [encrypt] and [decrypt].
+ */
 abstract class KryptoCipher(
-    val name: String,
+    /**
+     * Name of the cipher. It should be in the string resources in a file with a name
+     * corresponding to the English version of the cipher name.
+     */
+    @StringRes val name: Int,
+
+    /**
+     * Description of the cipher. It should be in the string resources in a file with a name
+     * corresponding to the English version of the cipher name.
+     */
     @StringRes val description: Int,
+
+    /**
+     * Cipher vector icon. Used when displaying cipher information.
+     */
     val icon: ImageVector
 ) {
 
+    /**
+     * This method encrypts a [message] with the help of a [key].
+     */
     abstract fun encrypt(message: String, key: String): String
 
+    /**
+     * This method decrypts a [message] with the help of a [key].
+     */
     abstract fun decrypt(message: String, key: String): String
 }
