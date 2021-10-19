b171317c2958f48841e6dfe27c3c694f88a4468c
VitasSalvantes <salvantesvitas@gmail.com>
2021-10-19 19:19:56 +0200

    Created entity

---
+++ app/src/main/java/com/vitassalvantes/krypto/data/Correspondence.kt
@@ -0,0 +1,17 @@
+package com.vitassalvantes.krypto.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Correspondence(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    @ColumnInfo(name = "correspondence_name")
+    val correspondenceName: String,
+    @ColumnInfo(name = "cipher_name")
+    val cipherName: Int,
+    @ColumnInfo(name = "key")
+    val key: String
+)
