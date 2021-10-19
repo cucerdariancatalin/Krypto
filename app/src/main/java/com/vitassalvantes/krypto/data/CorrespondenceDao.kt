0be478a71ac0a00f6cca728659c773a6def9f89f
VitasSalvantes <salvantesvitas@gmail.com>
2021-10-19 19:32:04 +0200

    Created DAO

---
+++ app/src/main/java/com/vitassalvantes/krypto/data/CorrespondenceDao.kt
@@ -0,0 +1,22 @@
+package com.vitassalvantes.krypto.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface CorrespondenceDao {
+    @Delete
+    suspend fun delete(correspondence: Correspondence)
+
+    @Insert
+    suspend fun insert(correspondence: Correspondence)
+
+    @Query("SELECT * FROM correspondence")
+    fun getAllCorrespondences(): Flow<List<Correspondence>>
+
+    @Query("SELECT * FROM correspondence WHERE id = :id")
+    fun getCorrespondence(id: Int): Flow<Correspondence>
+}
