449f458b4fafc7fc3f1ac85d446514fb23f7ef02
VitasSalvantes <salvantesvitas@gmail.com>
2021-10-19 19:25:21 +0200

    Added name to table

--- app/src/main/java/com/vitassalvantes/krypto/data/Correspondence.kt
+++ app/src/main/java/com/vitassalvantes/krypto/data/Correspondence.kt
@@ -4,7 +4,7 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(tableName = "correspondence")
 data class Correspondence(
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
