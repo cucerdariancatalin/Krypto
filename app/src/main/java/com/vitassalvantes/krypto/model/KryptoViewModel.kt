e4dbb73e2331169c4fa100d458fe42234c2ad10c
VitasSalvantes <salvantesvitas@gmail.com>
2021-10-24 11:24:56 +0200

    Created KryptoViewModelFactory and implemented methods to work with the database

--- app/src/main/java/com/vitassalvantes/krypto/model/KryptoViewModel.kt
+++ app/src/main/java/com/vitassalvantes/krypto/model/KryptoViewModel.kt
@@ -1,30 +1,83 @@
 package com.vitassalvantes.krypto.model
 
-import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
-import com.vitassalvantes.krypto.KryptoRoom
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.vitassalvantes.krypto.data.Correspondence
+import com.vitassalvantes.krypto.data.CorrespondenceDao
+import kotlinx.coroutines.launch
 
 /**
  * [KryptoViewModel] holds information about a all rooms and manages it.
  */
-class KryptoViewModel : ViewModel() {
-    /**
-     * List of all rooms
-     */
-    var listOfRooms = mutableStateListOf<KryptoRoom>()
-        private set
+class KryptoViewModel(private val correspondenceDao: CorrespondenceDao) : ViewModel() {
 
     /**
-     * Adding a new room to [listOfRooms]
+     * List of all correspondences
      */
-    fun addNewRoom(newRoom: KryptoRoom) {
-        listOfRooms.add(newRoom)
+    val listOfAllCorrespondences =
+        correspondenceDao.getAllCorrespondences().asLiveData()
+
+    /**
+     *
+     */
+    private fun addCorrespondence(correspondence: Correspondence) {
+        viewModelScope.launch {
+            correspondenceDao.insert(correspondence)
+        }
     }
 
     /**
-     * Removing a room from [listOfRooms]
+     *
      */
-    fun removeRoom(roomToRemove: KryptoRoom) {
-        listOfRooms.remove(roomToRemove)
+    private fun deleteCorrespondence(correspondence: Correspondence) {
+        viewModelScope.launch {
+            correspondenceDao.delete(correspondence = correspondence)
+        }
+    }
+
+    /**
+     *
+     */
+    fun addNewCorrespondence(
+        correspondenceName: String,
+        cipherName: Int,
+        key: String
+    ) {
+        addCorrespondence(
+            Correspondence(
+                correspondenceName = correspondenceName,
+                cipherName = cipherName,
+                key = key
+            )
+        )
+    }
+
+    /**
+     *
+     */
+    fun deleteCurrentCorrespondence(correspondence: Correspondence) {
+        deleteCorrespondence(correspondence = correspondence)
+    }
+
+    /**
+     *
+     */
+    fun findCorrespondenceById(id: Int): Correspondence =
+        listOfAllCorrespondences.value!!.find { it.id == id }!! // TODO: 24.10.2021 exception
+}
+
+/**
+ *
+ */
+class KryptoViewModelFactory(private val correspondenceDao: CorrespondenceDao) :
+    ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(KryptoViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return KryptoViewModel(correspondenceDao = correspondenceDao) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
     }
 }
