ea3d5e1e69c25f6a0cbf00376059673411d0919a
VitasSalvantes <salvantesvitas@gmail.com>
2021-10-25 19:05:08 +0200

    Removed unused parameters

--- app/src/main/java/com/vitassalvantes/krypto/navigation/KryptoScreen.kt
+++ app/src/main/java/com/vitassalvantes/krypto/navigation/KryptoScreen.kt
@@ -1,45 +1,34 @@
 package com.vitassalvantes.krypto.navigation
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Code
-import androidx.compose.material.icons.filled.List
-import androidx.compose.ui.graphics.vector.ImageVector
-import com.vitassalvantes.krypto.R
-
 /**
  * Class for navigation that contains all screens.
  *
  * Don't forget add new screens to [KryptoBottomAppBar] and [KryptoNavHost]!
  */
-sealed class KryptoScreen(val route: String, val iconId: ImageVector, val labelId: Int) {
+sealed class KryptoScreen(val route: String) {
 
     /**
-     * Object for navigation to [com.vitassalvantes.krypto.ui.screens.RoomsScreen].
+     * Object for navigation to [com.vitassalvantes.krypto.ui.screens.CorrespondencesScreen].
      */
-    object RoomsScreen : KryptoScreen("rooms_screen", Icons.Filled.List, R.string.rooms)
+    object CorrespondencesScreen : KryptoScreen("correspondences_screen")
 
     /**
      * Object for navigation to [com.vitassalvantes.krypto.ui.screens.CiphersScreen].
      */
-    object CiphersScreen : KryptoScreen("ciphers_screen", Icons.Filled.Code, R.string.ciphers)
+    object CiphersScreen : KryptoScreen("ciphers_screen")
 
     /**
-     * Object for navigation to [com.vitassalvantes.krypto.ui.screens.CreatingNewRoomScreen].
+     * Object for navigation to [com.vitassalvantes.krypto.ui.screens.CreatingNewCorrespondenceScreen].
      */
-    object CreatingNewRoom : KryptoScreen(
-        "creating_new_room_screen", Icons.Filled.Add, R.string.creating_a_new_room
-    )
+    object CreatingNewCorrespondence : KryptoScreen("creating_new_correspondence_screen")
 
     /**
      * Object for navigation to [com.vitassalvantes.krypto.ui.screens.CipherDetailsScreen].
      */
-    object CipherDetailsScreen :
-        KryptoScreen("cipher_details_screen", Icons.Filled.Add, R.string.cipher_details_screen)
+    object CipherDetailsScreen : KryptoScreen("cipher_details_screen")
 
     /**
-     * Object for navigation to [com.vitassalvantes.krypto.ui.screens.RoomDetailsScreen].
+     * Object for navigation to [com.vitassalvantes.krypto.ui.screens.CorrespondenceDetailsScreen].
      */
-    object RoomDetailsScreen :
-        KryptoScreen("room_details_screen", Icons.Filled.Add, R.string.room_details_screen)
+    object CorrespondenceDetailsScreen : KryptoScreen("correspondence_details_screen")
 }
