3df09363d0609b5eca1469a0b9cdf90a26f86784
VitasSalvantes <salvantesvitas@gmail.com>
2021-10-27 21:44:31 +0200

    Added ScreenContent and previews

--- app/src/main/java/com/vitassalvantes/krypto/ui/screens/CiphersScreen.kt
+++ app/src/main/java/com/vitassalvantes/krypto/ui/screens/CiphersScreen.kt
@@ -7,9 +7,11 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.vitassalvantes.krypto.ciphers.CiphersInfo.listOfAllCiphers
+import com.vitassalvantes.krypto.ciphers.KryptoCipher
 import com.vitassalvantes.krypto.navigation.KryptoScreen
 import com.vitassalvantes.krypto.ui.components.KryptoCard
 
@@ -18,6 +20,27 @@ import com.vitassalvantes.krypto.ui.components.KryptoCard
  */
 @Composable
 fun CiphersScreen(navController: NavHostController) {
+
+    CiphersScreenContent { cipher ->
+        navController.navigate(
+            KryptoScreen.CipherDetailsScreen.route + "/${listOfAllCiphers.indexOf(cipher)}"
+        ) {
+            // Avoid multiple copies of the same destination when
+            // reselecting the same item
+            launchSingleTop = true
+            // Restore state when reselecting a previously selected item
+            restoreState = true
+        }
+    }
+}
+
+/**
+ * UI content of the [CiphersScreen].
+ *
+ * @param onCardClickListener navigation to the [CipherDetailsScreen].
+ */
+@Composable
+fun CiphersScreenContent(onCardClickListener: (KryptoCipher) -> Unit) {
     LazyColumn(
         Modifier
             .fillMaxWidth()
@@ -28,16 +51,18 @@ fun CiphersScreen(navController: NavHostController) {
                 cardName = stringResource(id = cipher.name),
                 cardIcon = cipher.icon,
                 onClickListener = {
-                    navController.navigate(
-                        KryptoScreen.CipherDetailsScreen.route + "/${listOfAllCiphers.indexOf(cipher)}"
-                    ) {
-                        // Avoid multiple copies of the same destination when
-                        // reselecting the same item
-                        launchSingleTop = true
-                        // Restore state when reselecting a previously selected item
-                        restoreState = true
-                    }
-                })
+                    onCardClickListener(cipher)
+                }
+            )
         }
     }
+}
+
+@Preview(
+    name = "CiphersScreen",
+    showSystemUi = true
+)
+@Composable
+fun PreviewCiphersScreen() {
+    CiphersScreenContent {}
 }
