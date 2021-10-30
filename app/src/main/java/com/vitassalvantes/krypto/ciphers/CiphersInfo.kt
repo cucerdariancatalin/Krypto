5019b34df043bbb4e671e0ab1fd355812592958b
VitasSalvantes <salvantesvitas@gmail.com>
2021-10-30 19:54:32 +0200

    Removed method to get cipher by name

--- app/src/main/java/com/vitassalvantes/krypto/ciphers/CiphersInfo.kt
+++ app/src/main/java/com/vitassalvantes/krypto/ciphers/CiphersInfo.kt
@@ -1,7 +1,5 @@
 package com.vitassalvantes.krypto.ciphers
 
-import androidx.annotation.StringRes
-
 /**
  * Singleton to access the list of all implemented ciphers
  */
@@ -12,12 +10,4 @@ object CiphersInfo {
     val listOfAllCiphers = listOf<KryptoCipher>(
         CaesarCipher()
     )
-
-    /**
-     * Getting a cipher by its [com.vitassalvantes.krypto.ciphers.KryptoCipher.name]
-     */
-    fun getCipher(@StringRes cipherName: Int): KryptoCipher {
-        return listOfAllCiphers.find { it.name == cipherName } ?: listOfAllCiphers[0]
-        // TODO: 17.10.2021 throw CipherNotFoundException
-    }
 }
